a575a84435349648e45bce70c0b7d7f209b56622
DavydovDaniil30 <90439985+DavydovDaniil30@users.noreply.github.com>
2021-09-26 23:36:07 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+        public string Index { get; set; }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            
+            Address address = new Address();
+
+            
+            address.Country = "Ukraine";
+            address.City = "Kiev";
+            address.Street = "Lobachevskogo Street";
+            address.House = "23";
+            address.Apartment = "601";
+            address.Index = "11111";
+
+            
+            Console.WriteLine(address.Country);
+            Console.WriteLine(address.City);
+            Console.WriteLine(address.Street);
+            Console.WriteLine(address.House);
+            Console.WriteLine(address.Apartment);
+            Console.WriteLine(address.Index);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
